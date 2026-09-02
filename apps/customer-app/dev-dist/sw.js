b3438a31b38a7c38e753aadeaaf7b9cbc7a139cf
Om Parab <omparabomgp123@gmail.com>
2026-09-02 19:03:45 +0530

    chore: switch back to Atlas for MongoDB

--- apps/customer-app/dev-dist/sw.js
+++ apps/customer-app/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f3r5ao5mrkg"
+    "revision": "0.u3ed4436v9g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
